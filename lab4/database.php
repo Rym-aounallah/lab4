813350282394e575593ca4a5dc13741edbbce5d0
Rym-aounallah <rymaounallah17@gmail.com>
2025-05-23 15:10:17 +0100

    Add files via upload

---
+++ lab4/database.php
@@ -0,0 +1,17 @@
+<?php
+$host = 'localhost';
+$db = 'bmi_db';
+$user = 'root';
+$pass = '';
+$dsn = "mysql:host=$host;dbname=$db;charset=utf8";
+
+try {
+    $pdo = new PDO($dsn, $user, $pass);
+} catch (PDOException $e) {
+    echo json_encode([
+        'success' => false,
+        'message' => 'Database connection failed: ' . $e->getMessage()
+    ]);
+    exit;
+}
+?>
